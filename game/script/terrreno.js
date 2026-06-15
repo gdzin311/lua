7f9446bf013aa08e6d060fef375c2a4d948e33f9
gdzin311 <2025311407@aluno.riogrande.ifrs.edu.br>
2026-06-14 22:47:36 -0300

    gerar picos pronto

--- game/script/terrreno.js
+++ game/script/terrreno.js
@@ -1,27 +1,43 @@
 class terreno
 {
-    constructor(tela)
+    constructor(width, height, ctx)
     {
-        this.y = [];
+        this.ctx = ctx;
+
+        this.width = width;
+        this.height = height;
+
+        this.picos = [];
+        this.plataformas = [];
         this.displacement = 180;
-        this.power = tela;
-        this.a= [];
+        
+        //potencia de base 2 mais proxima da largura da tela para o algoritimo (Power of Two)
+        this.PoT = Math.pow(2, Math.ceil(Math.log(this.width) / Math.log(2)));
 
-
+        this.#gerar_picos();
+        this.#gerar_plataformas();
     }
 
-    #criar()
+    #gerar_picos()
     {
-        this.y[0] = 450;
-        this.y[this.power] = 450;
+        this.picos[0] = 450;
+        this.picos[this.PoT] = 450;
 
-        for (a = 1; a < d; a *= 2)
+        // intervalo entre +180 e -180 para criar a diferenca pós media dos vizinhos
+        let forca = (Math.random() * (this.displacement * 2)) - this.displacement;
+
+        for (let a = 1; a < this.PoT; a *= 2)
         {
-            for (b = d / a / 2; b < d; b += d / a)
+            for (let b = this.PoT / a / 2; b < this.PoT; b += this.PoT / a)
             {
-                this.y[b] = (this.y[b - d / a / 2] + this.y[b + d / a / 2]) / 2 + c.getRandomRange(-this.displacement, this.displacement);
+                this.picos[b] = (this.picos[b - this.PoT / a / 2] + this.picos[b + this.PoT / a / 2]) / 2 + forca
             }
             this.displacement *= 0.7;
         }
+    }
+
+    #gerar_plataformas()
+    {
+
     }
 }
